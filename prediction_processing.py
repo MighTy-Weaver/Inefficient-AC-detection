5285ccfcb43c4448018c49512754fb4c1f4a0616
Mighty Weaver <1874240442@qq.com>
2021-03-30 16:27:07 +0800

    update figure plotting

--- prediction_processing.py
+++ prediction_processing.py
@@ -3,6 +3,7 @@
 # Kindly notice that all the plots are default to save directly, it will not show out during the process.
 
 import json
+import os
 import pickle
 import warnings
 from math import sqrt
@@ -15,6 +16,7 @@ import shap
 from imblearn.over_sampling import SMOTE
 from matplotlib import cm
 from matplotlib.colors import ListedColormap
+from sklearn.metrics import r2_score
 from tqdm import tqdm
 
 # Ignore the warnings, let pandas print the full message and do some overall settings for matplotlib.
@@ -22,6 +24,8 @@ warnings.filterwarnings('ignore')
 room_list = pd.read_csv('summer_data_compiled.csv')['Location'].unique()
 plt.rc('font', family='Times New Roman')
 plt.rcParams["savefig.bbox"] = "tight"
+
+folder_name = 'Test_R2_2021_03_30_12_27_42'
 
 
 # Define our own original data loader
@@ -46,21 +50,20 @@ def original_dataloader(room: int):
 def prediction_dataloader(room: int):
     """This function is a prediction dataloader, it will return the real value, prediction value, accuracy and RMSE of
     the predictions along with the accuracy. It take the room number as the input parameter."""
-    data = pd.read_csv('./prediction.csv', index_col=None)
+    data = pd.read_csv('./{}/prediction.csv'.format(folder_name), index_col=None)
     data = data[data.room == room].reset_index(drop=True)
     real = np.array(json.loads(data.loc[0, 'real']))
     predict = np.array(json.loads(data.loc[0, 'predict']))
-    acc = sum([1 for i in range(len(predict)) if 0.9 * real[i] <= predict[i] <= 1.1 * real[i]]) / len(real)
+    r2 = r2_score(real, predict)
     rmse = sqrt(sum([(real[i] - predict[i]) ** 2 for i in range(len(real))]) / len(real))
-    return real + np.random.uniform(-0.05, 0.35, len(json.loads(data.loc[0, 'real']))), predict + np.random.uniform(
-        -0.1, 0.35, len(json.loads(data.loc[0, 'predict']))), rmse, acc
+    return real, predict, r2, rmse
 
 
 # This is the function to view the shapley additive explanation's importance plot.
 def view_shap_importance(room: int):
     """This function will plot the importance of the models by shapely additive explanations. It takes the room
     number as the input parameter."""
-    model = pickle.load(open('./models/{}.pickle.bat'.format(room), 'rb'))
+    model = pickle.load(open('./{}/models/{}.pickle.bat'.format(folder_name, room), 'rb'))
     explainer = shap.TreeExplainer(model)
     X, y = original_dataloader(room)
     shap_values = explainer.shap_values(X)
@@ -68,35 +71,33 @@ def view_shap_importance(room: int):
 
 
 # This is the function to calculate the RMSE value of a room, based on the model's predictions.
-def calculate_RMSE(room: int):
-    data = pd.read_csv('./prediction.csv', index_col=None)
+def calculate_R2_RMSE(room: int):
+    data = pd.read_csv('./{}/prediction.csv'.format(folder_name), index_col=None)
     data = data[data.room == room].reset_index(drop=True)
     real = json.loads(data.loc[0, 'real'])
     predict = json.loads(data.loc[0, 'predict'])
     square_sum = sum([(real[i] - predict[i]) ** 2 for i in range(len(real))])
-    correct = 0
-    for i in range(len(real)):
-        if 0.9 * real[i] <= predict[i] <= 1.1 * real[i]:
-            correct += 1
-    return float(correct / len(real)), sqrt(square_sum / len(real))
+    R2 = r2_score(real, predict)
+    return R2, sqrt(square_sum / len(real))
 
 
 # This function plots the interacted shapley value for each room's temperature and humidity.
 def plot_shap_interact(room: int):
     plt.rcParams.update({'font.size': 20})
-    model = pickle.load(open('./models/{}.pickle.bat'.format(room), 'rb'))
+    model = pickle.load(open('./{}/models/{}.pickle.bat'.format(folder_name, room), 'rb'))
     explainer = shap.TreeExplainer(model)
     X, y = original_dataloader(room)
     shap_values = explainer.shap_values(X)
     shap.dependence_plot("Temperature", shap_values, X, interaction_index="Humidity", save=True,
-                         path="./shap_TH_ac_plot/{}.png".format(room), show=False,
+                         path="./{}/shap_TH_ac_plot/{}.png".format(folder_name, room), show=False,
                          title="Shapley Value for Temperature & Humidity of Room {}".format(room))
+    plt.clf()
 
 
 # This is the function to plot the distribution of the predictions according to the ground truth.
 def plot_distribution(room: int):
     plt.rcParams.update({'font.size': 13})
-    real, predict, rmse, acc = prediction_dataloader(room)
+    real, predict, r2, rmse = prediction_dataloader(room)
     plt.rc('font', family='Times New Roman')
     # Define our own color bar for the plot.
     newcolors = cm.get_cmap('viridis', 256)(np.linspace(0, 1, 256))
@@ -114,41 +115,47 @@ def plot_distribution(room: int):
     plt.legend(frameon=False)
     plt.colorbar(label="Error (Real Value - Prediction)")
     plt.xlabel(
-        "Original AC\nOverall Accuracy: {}%\nRoot Mean Square Error: {}".format(round(100 * acc, 2), round(rmse, 4)))
-    plt.savefig("./distribution_plot/room{}.png".format(room))
+        "Original AC\nR2 score: {}\nRoot Mean Square Error: {}".format(round(r2, 4), round(rmse, 4)))
+    plt.savefig("./{}/distribution_plot/room{}.png".format(folder_name, room), bbox_inches='tight')
     # plt.show()
     plt.clf()
 
 
 # This is the function to plot the error and root mean square error distribution of all the rooms.
 def plot_error_distribution():
-    accuracy_list, rmse_list = [], []
+    r2_list, rmse_list = [], []
     # Looping through all the room and collect the statistics we need.
     for room_f in room_list:
         if room_f == 309 or room_f == 312 or room_f == 917 or room_f == 1001:
             continue
-        acc, rmse = calculate_RMSE(room_f)
-        accuracy_list.append(acc * 100)
+        r2, rmse = calculate_R2_RMSE(room_f)
+        r2_list.append(r2)
         rmse_list.append(rmse)
     plt.rcParams.update({'font.size': 15})
     # Use the historgram in matplotlib to plot the accuracy distribution histogram.
-    plt.hist(accuracy_list, bins=50, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
-    plt.xlabel("Accuracy(%)\nMean Accuracy: {}%".format(round(mean(accuracy_list), 2)))
+    plt.hist(r2_list, bins=50, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
+    plt.xlabel("R2 Score\nMean R2 Score: {}".format(round(mean(r2_list), 2)))
     plt.ylabel("Occurrence")
-    plt.title("The Accuracy Distribution Histogram")
-    plt.savefig('./AccDis.png')
+    plt.title("The R2 Score Distribution Histogram")
+    plt.savefig('./{}/R2Dis.png'.format(folder_name), bbox_inches='tight')
     plt.clf()
     # Use the same to plot the root mean square error distribution histogram.
     plt.hist(rmse_list, bins=40, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
     plt.xlabel("Root Mean Square Error\nMean RMSE: {}".format(round(mean(rmse_list), 4)))
     plt.ylabel("Occurrence")
     plt.title("The RMSE Distribution Histogram")
-    plt.savefig('./RMSEDis.png')
+    plt.savefig('./{}/RMSEDis.png'.format(folder_name), bbox_inches='tight')
     plt.clf()
 
 
 # The main function
 if __name__ == "__main__":
+    if not os.path.exists('./{}/shap_TH_ac_plot/'.format(folder_name)):
+        os.mkdir('./{}/shap_TH_ac_plot/'.format(folder_name))
+    if not os.path.exists('./{}/distribution_plot/'.format(folder_name)):
+        os.mkdir('./{}/distribution_plot/'.format(folder_name))
+    if not os.path.exists('./{}/SMOTE_room/'.format(folder_name)):
+        os.mkdir('./{}/SMOTE_room/'.format(folder_name))
     for room in tqdm(room_list):
         # Delete the rooms with low quality data manually.
         if room == 309 or room == 312 or room == 917 or room == 1001:
