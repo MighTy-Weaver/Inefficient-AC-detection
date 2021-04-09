0f719cf39c3d3536e401d0921584cdea95669c67
Mighty Weaver <1874240442@qq.com>
2021-04-09 14:17:03 +0800

    update

--- training_xgboost_model.py
+++ training_xgboost_model.py
@@ -12,8 +12,7 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import KFold
+from sklearn.utils import compute_sample_weight
 from tqdm import tqdm
 from xgboost import DMatrix, cv
 
@@ -21,7 +20,7 @@ from xgboost import DMatrix, cv
 parser = argparse.ArgumentParser()
 parser.add_argument("--metric", choices=['1-R2', 'RMSE', '10acc', 'MSE'], type=str, required=False, default='1-R2',
                     help="The objective metric you want to use to train the XGBoost model")
-parser.add_argument("--log", choices=[0, 1, 100], type=int, required=False, default=False,
+parser.add_argument("--log", choices=[0, 1, 100], type=int, required=False, default=0,
                     help="Whether to print out the training progress")
 args = parser.parse_args()
 
@@ -87,49 +86,29 @@ for room in tqdm(data['Location'].unique()):
     # We extract the data of particular room and run the SMOTE algorithm on it.
     room_data = data[data.Location == room]
 
-    # Label all the AC data by 0.7, all AC above 0.7 will be marked as 1, otherwise 0. Split into X and y
-    room_data['SMOTE_split'] = (room_data['AC'] > 0.7).astype('int')
-    X = room_data.drop(['SMOTE_split'], axis=1)
-    y = room_data['SMOTE_split']
-
-    # Run the SMOTE algorithm and retrieve the result.
-    model_smote = SMOTE(random_state=621, k_neighbors=3)
-    room_data_smote, smote_split = model_smote.fit_resample(X, y)
-
-    # concat the result from SMOTE and split the result into X and y for training.
-    room_data_smote = pd.concat([room_data_smote, smote_split], axis=1)
-    y = room_data_smote['AC']
-    X = room_data_smote.drop(['AC', 'Location', 'SMOTE_split'], axis=1)
+    # Apply the sample weight strategy to the AC value
+    class_sample = pd.cut(room_data['AC'], bins=10)
+    weight = compute_sample_weight(class_weight="balanced", y=class_sample)
+    y = room_data['AC']
+    X = room_data.drop(['AC', 'Location'], axis=1)
 
     # setup our training parameters and a model variable as model checkpoint
     param_dict = {'objective': 'reg:squarederror', 'max_depth': 10, 'reg_alpha': 7, 'min_child_weight': 0.1,
                   'colsample_bytree': 0.8, 'learning_rate': 0.06}
-    xgb_model = None
 
-    # We use KFold in scikit-learn as our cross-validation mechanism. 10 folds is used for each room.
-    for train, test in KFold(n_splits=10, shuffle=True, random_state=621).split(X, y):
-        # Build the input and output matrix for training in each fold and setup the watchlist for training validation.
-        input_matrix = DMatrix(data=X.loc[train], label=y.loc[train])
-        test_matrix = DMatrix(data=X.loc[test], label=y.loc[test])
-        watchlist = [(test_matrix, 'eval'), (input_matrix, 'train')]
-
-        # If there is already a checkpoint model, we'll keep training the checkpoint,
-        # Otherwise, it will train from the beginning. It will ultimately bring us the cross-validated xgboost model.
-        if xgb_model is None:
-            xgb_model = xgb.train(params=param_dict, dtrain=input_matrix, num_boost_round=100, evals=watchlist,
-                                  feval=objective_dict[args.metric], maximize=True, xgb_model=None,
-                                  verbose_eval=args.log)
-        else:
-            xgb_model = xgb.train(params=param_dict, dtrain=input_matrix, num_boost_round=100, evals=watchlist,
-                                  feval=objective_dict[args.metric], maximize=True, xgb_model=xgb_model,
-                                  verbose_eval=args.log)
-    pickle.dump(xgb_model, open('./{}/models/{}.pickle.bat'.format(log_folder_name, room), 'wb'))
+    X = X.to_numpy()
 
     # Build another full data matrix for the built-in cross validation function to work.
-    data_matrix = DMatrix(data=X, label=y)
+    data_matrix = DMatrix(data=X, label=y, weight=weight)
     # Use the built-in cv function to do the cross validation, still with ten folds, this will return us the results.
     xgb_cv_result = cv(dtrain=data_matrix, params=param_dict, as_pandas=True, num_boost_round=300, nfold=10,
                        shuffle=True, seed=621, feval=objective_dict[args.metric], maximize=True)
+
+    watchlist = [(data_matrix, 'eval'), (data_matrix, 'train')]
+    xgb_model = xgb.train(params=param_dict, dtrain=data_matrix, num_boost_round=100, evals=watchlist,
+                          verbose_eval=args.log, feval=objective_dict[args.metric])
+    pickle.dump(xgb_model, open('./{}/models/{}.pickle.bat'.format(log_folder_name, room), 'wb'))
+
     xgb_cv_result['room'] = room
     error_csv.loc[len(error_csv)] = xgb_cv_result.loc[len(xgb_cv_result) - 1]
 
