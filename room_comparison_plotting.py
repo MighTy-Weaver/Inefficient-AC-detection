83c6ae12304e8161c8f68c466b6f9f523ccfad80
Mighty Weaver <1874240442@qq.com>
2021-06-16 14:15:21 +0800

    upload data description plot

--- room_comparison_plotting.py
+++ room_comparison_plotting.py
@@ -1,65 +1,83 @@
 import warnings
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot as plt
 
-# Ignore all the warnings
+folder_name = "20210616130636_cate2_KMeans"
+mode = "CLU"  # "CSV" or "CLU"
+categories = ["High", "Low"]
+
+# Ignore all the warnings and Set some general settings of all the matplotlib.
 warnings.filterwarnings('ignore')
-
-numpy_file_name = ""
-
-lists = np.load('./{}'.format(numpy_file_name), allow_pickle=True).item()
-num_cates = len(lists)
-
-# Read the original data
-data = pd.read_csv('./summer_data_compiled.csv')
-data = data[(data.AC > 0) & (data.Prev_1hr)]
-
-room_split = pd.read_csv('./room_classification.csv', index_col=None)
-
-# Read the List of all rooms and classify them into three efficient list by human observation
-room_list = data['Location'].unique()
-efficient_list = room_split[room_split['Room AC Status- Lu-20210521'] == 'High efficiency']['Room Number']
-moderate_list = room_split[room_split['Room AC Status- Lu-20210521'] == 'Moderate efficiency']['Room Number']
-inefficient_list = room_split[room_split['Room AC Status- Lu-20210521'] == 'Low efficiency']['Room Number']
-
-# create three dataframe list to store the dataframes
-high_list = [data[data.Location == i] for i in efficient_list]
-mid_list = [data[data.Location == i] for i in moderate_list]
-low_list = [data[data.Location == i] for i in inefficient_list]
-
-# Concat three lists into three dataframes
-high_df = pd.concat(high_list)
-mid_df = pd.concat(mid_list)
-low_df = pd.concat(low_list)
-
-# Set some general settings of all the matplotlib.
 plt.rcParams.update({'font.size': 22})
 plt.rc('font', family='Times New Roman')
 
-# Calculate the mean of the data
-high_mean, mid_mean, low_mean = round(np.array(high_df['AC']).mean(), 4), round(np.array(mid_df['AC']).mean(),
-                                                                                4), round(np.array(low_df['AC']).mean(),
-                                                                                          4)
-# Filter out the data less than 1.5
-high_df = high_df[high_df.AC < 1.5]
-mid_df = mid_df[mid_df.AC < 1.5]
-low_df = low_df[low_df.AC < 1.5]
+colors = ['brown', 'darkblue', 'skyblue']
 
-# Plot the distribution plot.
-sns.distplot(high_df['AC'], bins=sorted(high_df['AC'].unique()),
-             label="High efficiency, mean={}kWh, #data{}".format(high_mean, len(high_df)), color="brown",
-             hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
-sns.distplot(mid_df['AC'], bins=sorted(mid_df['AC'].unique()),
-             label="Moderate efficiency, mean={}kWh, #data{}".format(mid_mean, len(mid_df)), color="darkblue",
-             hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
-sns.distplot(low_df['AC'], bins=sorted(low_df['AC'].unique()),
-             label="Low efficiency, mean={}kWh, #data{}".format(low_mean, len(low_df)), color="skyblue",
-             hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
+# Read the original data
+data = pd.read_csv('./summer_data_compiled.csv', index_col=0)
+print(len(data))
+data = data[(data.AC > 0) & (data.Prev_1hr)]
+print(len(data))
 
-plt.legend()
-plt.xlabel("Hourly AC Electricity Consumption/kWh")
-plt.ylabel("Kernel Density")
-plt.show()
+if mode == "CLU":
+    lists = np.load('{}/category.npy'.format(folder_name), allow_pickle=True)
+    num_cates = len(lists)
+
+    dataframes = [[data[data.Location == int(i)] for i in lis] for lis in lists]
+    dataframe_list = [pd.concat(dataframes[i]) for i in range(num_cates)]
+    mean_list = [round(np.array(df['AC']).mean(), 4) for df in dataframe_list]
+    print(mean_list)
+
+    for i in range(num_cates):
+        sns.distplot(dataframe_list[i]['AC'], bins=sorted(dataframe_list[i]['AC'].unique()),
+                     label="{} efficiency, mean={}kWh, #data={}".format(categories[i], mean_list[i],
+                                                                        len(dataframe_list[i])), color=colors[i],
+                     hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
+    plt.legend()
+    plt.xlabel("Hourly AC Electricity Consumption/kWh")
+    plt.ylabel("Kernel Density")
+    plt.show()
+
+elif mode == "CSV":
+    room_split = pd.read_csv('./room_classification.csv', index_col=None)
+
+    # Read the List of all rooms and classify them into three efficient list by human observation
+    room_list = data['Location'].unique()
+    efficient_list = room_split[room_split['Room AC Status- Lu-20210521'] == 'High efficiency']['Room Number']
+    moderate_list = room_split[room_split['Room AC Status- Lu-20210521'] == 'Moderate efficiency']['Room Number']
+    inefficient_list = room_split[room_split['Room AC Status- Lu-20210521'] == 'Low efficiency']['Room Number']
+
+    # create three dataframe list to store the dataframes
+    high_list = [data[data.Location == i] for i in efficient_list]
+    mid_list = [data[data.Location == i] for i in moderate_list]
+    low_list = [data[data.Location == i] for i in inefficient_list]
+
+    # Concat three lists into three dataframes
+    high_df = pd.concat(high_list)
+    mid_df = pd.concat(mid_list)
+    low_df = pd.concat(low_list)
+
+    # Calculate the mean of the data
+    high_mean, mid_mean, low_mean = round(np.array(high_df['AC']).mean(), 4), round(np.array(mid_df['AC']).mean(),
+                                                                                    4), round(
+        np.array(low_df['AC']).mean(), 4)
+    print(high_mean, mid_mean, low_mean)
+
+    # Plot the distribution plot.
+    sns.distplot(high_df['AC'], bins=sorted(high_df['AC'].unique()),
+                 label="High efficiency, mean={}kWh, #data{}".format(high_mean, len(high_df)), color="brown",
+                 hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
+    sns.distplot(mid_df['AC'], bins=sorted(mid_df['AC'].unique()),
+                 label="Moderate efficiency, mean={}kWh, #data{}".format(mid_mean, len(mid_df)), color="darkblue",
+                 hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
+    sns.distplot(low_df['AC'], bins=sorted(low_df['AC'].unique()),
+                 label="Low efficiency, mean={}kWh, #data{}".format(low_mean, len(low_df)), color="skyblue",
+                 hist_kws={"edgecolor": "black"}, kde_kws={"linewidth": "3"})
+
+    plt.legend()
+    plt.xlabel("Hourly AC Electricity Consumption/kWh")
+    plt.ylabel("Kernel Density")
+    plt.show()
--- shapley_cluster.py
+++ shapley_cluster.py
@@ -1,39 +1,39 @@
 import os
+from datetime import datetime
 from glob import glob
 from shutil import copyfile
 
 import cv2
 import numpy as np
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
+categories = 3
+available_cates = ["KMeans", "MiniKMeans"]
+cate_name = "KMeans"
+
+folder_name = '{}_cate{}_{}/'.format(datetime.now().strftime("%Y%m%d%H%M%S"), categories, cate_name)
+cate_symbol = ['a', 'b', 'c', 'd']
+cates = [[] for _ in range(categories)]
 
 images = glob('./Test_R2_HYPEROPT_SMOTE/shap_T_ac_plot/*.png')
 room_list = [img.split('\\')[-1].split('.')[0] for img in images]
 image_list = np.array([cv2.imread(img) for img in images])
 image_list = image_list.reshape(len(image_list), -1)
-print(room_list)
-print(images)
 
-kmeans = MiniBatchKMeans(4)
-kmeans.fit(image_list)
-print(kmeans.labels_)
-cate_a, cate_b, cate_c, cate_d = [], [], [], []
+if cate_name == "KMeans":
+    cluster = KMeans(categories)
+elif cate_name == "MiniKMeans":
+    cluster = MiniBatchKMeans(categories)
+else:
+    cluster = KMeans(categories)
+cluster.fit(image_list)
+print(cluster.labels_)
 
-os.mkdir('catea/')
-os.mkdir('cateb/')
-os.mkdir('catec/')
-os.mkdir('cated/')
-for ind, i in enumerate(kmeans.labels_):
-    if i == 0:
-        cate_a.append(room_list[ind])
-        copyfile(images[ind], 'catea/{}.png'.format(room_list[ind]))
-    if i == 1:
-        cate_b.append(room_list[ind])
-        copyfile(images[ind], 'cateb/{}.png'.format(room_list[ind]))
-    if i == 2:
-        cate_c.append(room_list[ind])
-        copyfile(images[ind], 'catec/{}.png'.format(room_list[ind]))
-    if i == 3:
-        cate_d.append(room_list[ind])
-        copyfile(images[ind], 'cated/{}.png'.format(room_list[ind]))
-category = {1: cate_a, 2: cate_b, 3: cate_c, 4: cate_d}
-np.save('./category.npy', category)
+os.mkdir('{}/'.format(folder_name))
+for i in range(categories):
+    os.mkdir('{}/{}'.format(folder_name, cate_symbol[i]))
+for ind, i in enumerate(cluster.labels_):
+    cates[i].append(room_list[ind])
+    copyfile(images[ind], '{}/{}/{}.png'.format(folder_name, cate_symbol[i], room_list[ind]))
+
+np.save('./{}/category.npy'.format(folder_name), cates)
